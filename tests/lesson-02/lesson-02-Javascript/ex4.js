3f30a6f7028ef410eb7e8e2d0265fb85c238ce4f
Tam Nghiem <tam.306201@gmail.com>
2026-06-09 23:12:20 +0700

    feat: add file ex3->9

---
+++ tests/lesson-02/lesson-02-Javascript/ex4.js
@@ -0,0 +1,11 @@
+const chieuCao = 52;
+
+if (chieuCao > 100) {
+
+    console.log(`Cân nặng lý tưởng, tối đa và tối thiểu của bạn lần lượt là: ${(chieuCao-100)*9/10} kg, ${chieuCao-100} kg, ${(chieuCao-100)*8/10} kg`);
+
+} else {
+
+    console.log("Công thức chỉ áp dụng cho chiều cao trên 100 cm");
+
+}
